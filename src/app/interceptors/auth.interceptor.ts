15bd9dd65022ba0acc1d4890501d78d638336468
MishelEspinoza07 <102001751+MishelEspinoza07@users.noreply.github.com>
2024-11-05 23:08:11 -0400

    [04/11/2024] Auth interceptor

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,7 +1,3 @@
-/*import { HttpInterceptorFn } from '@angular/common/http';
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};*/
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { AuthService } from "../auth.service";
